4acca22c4b52ff3e5bf77d0f3520ea402ca1d443
syn <vicdemilly@gmail.com>
2019-04-11 09:25:01 +0200

    Ajout de commentaires

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function formulaire()
+// On ajoute la fonction nommée dans le onclick pour l'activer lors du clic
+// Le calcul que nous voulons faire est décrit dans le alert
+function compute()
 {
   let nbr1 = firstNumber.value
   let nbr2 = secondNumber.value
