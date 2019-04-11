92021eea9e7c4df35aa094627331e984d90c9ba4
syn <vicdemilly@gmail.com>
2019-04-11 14:39:04 +0200

    Ajout des ;

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 // Le calcul que nous voulons faire est décrit dans le alert
 function compute()
 {
-  let nbr1 = document.getElementById('firstNumber').value
-  let nbr2 = document.getElementById('secondNumber').value
-  alert(Math.trunc(nbr1) * (nbr2))
+  let nbr1 = document.getElementById('firstNumber').value;
+  let nbr2 = document.getElementById('secondNumber').value;
+  alert(Math.trunc(nbr1) * (nbr2));
 }
 // Le trunc permet de dire "ne prends pas en compte le nombre après la virgule du premier champs"
 // C'est une méthode de l'objet Math
